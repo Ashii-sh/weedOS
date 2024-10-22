9e88c11565216c16df541b1744a4d48cd6c78a70
AshleyKSH <149899656+Ashii-sh@users.noreply.github.com>
2024-10-22 17:43:46 -0400

    Update install-script-amd64.sh 0.1.3
    forgot the -y parameter on line 14

--- install-script-amd64.sh
+++ install-script-amd64.sh
@@ -1,5 +1,5 @@
 clear
-echo "WeedOS II v0.1.2"
+echo "WeedOS II v0.1.3"
 echo "Made in FL < 3"
 echo ""
 echo "Starting in 3 seconds..."
@@ -11,7 +11,7 @@ clear
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install wget git unzip firmware-iwlwifi firmware-realtek mesa-utils cpufrequtils dbus-x11 -y
-sudo apt install xserver-xorg xinit plymouth plymouth-themes lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 plank picom xfce4-whiskermenu-plugin 
+sudo apt install xserver-xorg xinit plymouth plymouth-themes lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 plank picom xfce4-whiskermenu-plugin -y
 
 echo ""
 echo "END" 
