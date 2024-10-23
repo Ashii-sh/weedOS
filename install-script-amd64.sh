8b2d924fd0b75366dd6eb6adcf838cb70d3a6d39
AshleyKSH <149899656+Ashii-sh@users.noreply.github.com>
2024-10-22 20:47:12 -0400

    Update install-script-amd64.sh 0.1.4
    Added Global Menu Enabler from Heterochromia420

--- install-script-amd64.sh
+++ install-script-amd64.sh
@@ -1,5 +1,5 @@
 clear
-echo "WeedOS II v0.1.3"
+echo "WeedOS II v0.1.4"
 echo "Made in FL < 3"
 echo ""
 echo "Starting in 3 seconds..."
@@ -8,10 +8,14 @@ clear
 
 ## Stage 1 
 
+WEEDOSDIRROOT=${PWD}
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install wget git unzip firmware-iwlwifi firmware-realtek mesa-utils cpufrequtils dbus-x11 -y
-sudo apt install xserver-xorg xinit plymouth plymouth-themes lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 plank picom xfce4-whiskermenu-plugin -y
+sudo apt install xserver-xorg xinit plymouth plymouth-themes lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 plank picom xfce4-whiskermenu-plugin fonts-roboto xfce4-terminal -y
+sudo apt install xfce4-appmenu-plugin vala-panel-appmenu appmenu* -y --no-install-recommends --no-install-suggests 
+xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true
+xfconf-query -c xsettings -p /Gtk/ShellShowsAppmenu -n -t bool -s true
+sudo cp ${WEEDOSDIRROOT}/Assets/52appmenu-gtk-module_add-to-gtk-modules /etc/X11/Xsession.d
+sudo apt remove uxterm xterm
 
-echo ""
-echo "END" 
