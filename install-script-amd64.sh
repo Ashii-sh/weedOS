6db6e1e4b8ba512bb996baa6ccc4f96f5326ca9a
AshleyKSH <149899656+Ashii-sh@users.noreply.github.com>
2024-10-31 22:57:06 -0400

    Update install-script-amd64.sh
    0.1.8

--- install-script-amd64.sh
+++ install-script-amd64.sh
@@ -1,5 +1,5 @@
 clear
-echo "WeedOS II Beta v0.1.7"
+echo "WeedOS II Beta v0.1.8"
 echo "Made in FL < 3"
 echo ""
 echo "Starting in 3 seconds..."
@@ -12,7 +12,7 @@ WEEDOSDIRROOT=${PWD}
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install wget git unzip firmware-iwlwifi firmware-realtek mesa-utils cpufrequtils dbus-x11 -y
-sudo apt install xserver-xorg xinit plymouth plymouth-themes lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 plank picom xfce4-whiskermenu-plugin fonts-roboto xfce4-terminal mugshot -y
+sudo apt install xserver-xorg xinit plymouth plymouth-themes lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 plank picom  xfce4-whiskermenu-plugin xfce4-power-manager-plugins fonts-roboto xfce4-terminal mugshot qt5ct -y
 sudo apt install xfce4-appmenu-plugin vala-panel-appmenu appmenu* -y --no-install-recommends --no-install-suggests 
 xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true
 xfconf-query -c xsettings -p /Gtk/ShellShowsAppmenu -n -t bool -s true
@@ -36,3 +36,28 @@ sudo cp -r numix-icon-theme-circle/Numix* ~/.icons
 sudo cp -r numix-icon-theme-circle/Numix* /usr/share/icons
 sudo cp -r numix-icon-theme/Numix* ~/.icons
 sudo cp -r numix-icon-theme/Numix* /usr/share/icons
+cp ${WEEDOSDIRROOT}/Assets/WeedOS-Themes.zip ${WEEDOSDIRROOT}/cache
+cd cache/
+unzip WeedOS-Themes.zip
+cp -r WeedOS* /usr/share/themes
+cp -r WeedOS* ~/.themes
+cd ..
+rm -rf 
+sudo cp -r ~/.themes/WeedOS/gtk-4.0/* ~/.config/gtk-4.0/
+cd ${WEEDOSDIRROOT}/cache
+wget https://github.com/catppuccin/cursors/releases/download/v1.0.1/catppuccin-latte-light-cursors.zip
+unzip catppuccin-latte-light-cursors.zip
+cp -r catppuccin-latte-light-cursors/ ~/.icons
+cp -r catppuccin-latte-light-cursors/ /usr/share/icons
+cd ..
+sudo rm -rf ${WEEDOSDIRROOT}/cache/*
+
+## Stage 3 
+
+sudo apt install chromium vlc xfce4-screenshooter mousepad gnome-software flatpak gnome-software-plugin-flatpak -y
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+
+sudo apt remove uxterm xterm xfce4-appfinder
+
+
